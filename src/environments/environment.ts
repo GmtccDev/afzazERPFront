4e6bc62f18f73af2e2d24b77521b42f62154bcd8
Mahmoud Abd-Elradi <Mahmoud Abd-Elradi@DESKTOP-FMV3U30>
2023-09-13 09:13:32 +0200

    5031

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,5 @@
 export const environment = {
 apiUrl:"http://localhost:5031",
-//apiUrl:"http://195.88.87.244:2525",
-// apiUrl:"http://195.88.87.244:5050",
   production: false
   };
-  //apiUrl:"http://mahmoudabd2345-002-site5.atempurl.com",
+
