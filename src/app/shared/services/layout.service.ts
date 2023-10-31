f61769eed4f6855769d3434913a0ce277f4a15c5
rania-nasr77 <ranianasrabdo@gmail.com>
2023-10-31 13:14:38 +0200

    update-UI-31-oct

--- src/app/shared/services/layout.service.ts
+++ src/app/shared/services/layout.service.ts
@@ -1,30 +1,43 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class LayoutService {
 
-  public config = {
-    settings: {
-      layout: 'Dubai',
-      layout_type: 'ltr',
-      layout_version: 'light-only',
-      sidebar_type: 'default-sidebar'
-    },
-    color: {
-      primary_color: '#4bc2f1',
-      secondary_color: '#575656'
-    }
-  }
+	//   public config = {
+	//     settings: {
+	//       layout: 'Dubai',
+	//       layout_type: 'ltr',
+	//       layout_version: 'light-only',
+	//       sidebar_type: 'default-sidebar'
+	//     },
+	//     color: {
+	//       primary_color: '#4bc2f1',
+	//       secondary_color: '#575656'
+	//     }
+	//   }
 
-  constructor() {
-    if (this.config.settings.layout_type == 'rtl')
-      document.getElementsByTagName('html')[0].setAttribute('dir', this.config.settings.layout_type);
+	public config = {
+		settings: {
+			layout: 'Seoul',
+			layout_type: 'rtl',
+			layout_version: 'light-only',
+			sidebar_type: 'default-sidebar',
+		},
+		color: {
+			primary_color: '#4bc2f1',
+			secondary_color: '#575656'
+		}
+	}
 
-    document.documentElement.style.setProperty('--theme-deafult', this.config.color.primary_color);
-    document.documentElement.style.setProperty('--theme-secondary', this.config.color.secondary_color);
-  }
+	constructor() {
+		if (this.config.settings.layout_type == 'rtl')
+			document.getElementsByTagName('html')[0].setAttribute('dir', this.config.settings.layout_type);
+
+		document.documentElement.style.setProperty('--theme-deafult', this.config.color.primary_color);
+		document.documentElement.style.setProperty('--theme-secondary', this.config.color.secondary_color);
+	}
 
 
 }
