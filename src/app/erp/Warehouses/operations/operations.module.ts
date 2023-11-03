4e2dd5d02db58f679cc568cf8c158ba91a21e64d
HP <HP@DESKTOP-C14OOKS>
2023-11-03 20:29:08 +0200

    update new

--- src/app/erp/Warehouses/operations/operations.module.ts
+++ src/app/erp/Warehouses/operations/operations.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule, DatePipe } from '@angular/common';
 import {SharedModule} from '../../../shared/shared.module'
 import { NgxSpinnerModule } from 'ngx-spinner';
-import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzTableModule } from 'ng-zorro-antd/table';   
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { OperationsRoutingModule } from './opertations-routing.module';
