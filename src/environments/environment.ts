087d3fcad9989e01d42b8b594290c79875ff8d12
rania-nasr77 <ranianasrabdo@gmail.com>
2023-11-20 15:35:35 +0200

    update-UI-20-nov

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
 	// apiUrl: "http://195.88.87.244:6988",
-	 apiUrl: "http://localhost:5030",
-	//apiUrl: "http://195.88.87.244:9188",
+	//  apiUrl: "http://localhost:5030",
+	apiUrl: "http://195.88.87.244:9188",
 	// apiUrl: "http://localhost:5030",
 	production: true,
 	timeoutInMinutes: 5
